524158a22a375ca66d6ac2f409e462245c88b6be
Rinmal <121992801+Rinmal@users.noreply.github.com>
2023-01-22 06:53:52 +0000

    [Add] いいね機能の追加

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,22 @@
+class FavoritesController < ApplicationController
+
+  def create
+    book = Book.find(params[:book_id])
+    if favorite = current_user.favorites.new(book_id: book.id)
+      favorite.save
+      redirect_back fallback_location: root_path
+    else
+      redirect_to root_path, notice: "You're failed to favorite"
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: book.id)
+    if favorite.destroy
+      redirect_back fallback_location: root_path
+    else
+      redirect_to root_path, notice: "You're failed to cancel"
+    end
+  end
+end
