05a434e9efca0263ef3b0720cbfe6717293eab4b
Rinmal <121992801+Rinmal@users.noreply.github.com>
2023-01-25 08:21:58 +0000

    [Update] コメント機能の非同期通信化

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,23 +1,18 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    if comment.save
-     redirect_to book_path(book)
-    else
-      @user = User.find(book.user_id)
-      @book = Book.find(book.id)
-      @book_comments = BookComment.all
-      @book_comment = comment
-      render ("books/show")
-    end
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    @book_comments = BookComment.all
+    @comment.save
+    @book_comment = BookComment.new
   end
 
   def destroy
     BookComment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
+    @book_comments = BookComment.all
+    @book_comment = BookComment.new
   end
 
   private
