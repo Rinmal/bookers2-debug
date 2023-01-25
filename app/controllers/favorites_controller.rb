1bada38d4d4386423520ca374472a31467681587
Rinmal <121992801+Rinmal@users.noreply.github.com>
2023-01-25 05:56:43 +0000

    [Update] いいね機能の非同期化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,22 +1,14 @@
 class FavoritesController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
-    if favorite.save
-      redirect_back fallback_location: root_path
-    else
-      redirect_to root_path, notice: "You're failed to favorite"
-    end
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: @book.id)
+    favorite.save
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
-    if favorite.destroy
-      redirect_back fallback_location: root_path
-    else
-      redirect_to root_path, notice: "You're failed to cancel"
-    end
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
+    favorite.destroy
   end
 end
